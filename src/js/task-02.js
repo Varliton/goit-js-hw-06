4001def8697e93c49bf95fb97c3ff2553763be54
Varliton <nbmusic2405@gmail.com>
2022-12-10 23:25:52 +0200

    сделал второй таск

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const listRefs = ingredients.map(ingredient => {
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+  return listEl;
+})
+ingredientsRef.append(...listRefs);
