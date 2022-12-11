8467ccc1fc7323a471ad456a370b0541d3e8fb86
Varliton <nbmusic2405@gmail.com>
2022-12-11 15:56:13 +0200

    Сделал восьмой таск

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = {};
+    formData.forEach((name, value) => {
+      if (!name || !value) {
+        alert('Please fill in the fields');
+      } else {
+        data[value] = name;
+      }
+    });
+    if (data.email && data.password) {
+      console.log(data);
+      event.currentTarget.reset();
+    }
+}
