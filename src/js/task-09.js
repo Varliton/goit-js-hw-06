2f5aa52161dd3486612cb42111546ac49aa254cc
Varliton <nbmusic2405@gmail.com>
2022-12-11 16:34:13 +0200

    сделал девятый таск

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const bodyRef = document.querySelector('body');
+const colorRef = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
+buttonRef.addEventListener('click', onBackgroundColorChange)
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function onBackgroundColorChange(event) {
+  event.preventDefault();
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorRef.textContent = getRandomHexColor()
 }
